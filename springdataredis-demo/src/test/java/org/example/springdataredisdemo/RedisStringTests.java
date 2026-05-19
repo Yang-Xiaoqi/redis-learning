9a1526a239f8abcbc6138fc9fb55cfad97667f04
yangxiaoqi <2200372789@qq.com>
2026-05-19 12:14:35 +0800

    StringRedisTemplate

---
+++ springdataredis-demo/src/test/java/org/example/springdataredisdemo/RedisStringTests.java
@@ -0,0 +1,41 @@
+package org.example.springdataredisdemo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.springdataredisdemo.redis.pojo.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+@SpringBootTest
+class RedisStringTests {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    @Test
+    void testString(){
+        //写入一条string数据
+        stringRedisTemplate.opsForValue().set("name","琦哥");
+
+        Object name = stringRedisTemplate.opsForValue().get("name");
+        System.out.println("name = " + name);
+    }
+    private static final ObjectMapper mapper = new ObjectMapper();
+    @Test
+    void testSaveUser() throws JsonProcessingException {
+
+        User user = new User("虎哥",21);
+        //手动序列化
+        String json = mapper.writeValueAsString(user);
+        stringRedisTemplate.opsForValue().set("user:200",json);
+
+        String jsonUser = stringRedisTemplate.opsForValue().get("user:200");
+        User user1=mapper.readValue(jsonUser,User.class);
+        System.out.println(user1);
+    }
+
+}
